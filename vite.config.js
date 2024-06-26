85e8f39274465983e2412caf1060925f13b2c176
navaneeth <navaneeth@example.com>
2024-06-26 17:15:15 +0530

    routing issue  commit

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
 
-//  for cors issue
-server: {
-  proxy: {
-      '/LaptopAPI': {
-          target: 'http://localhost:8081',
-          changeOrigin: true,
-          secure: false,
-         // rewrite: (path) => path.replace(/^\/LaptopAPI/, '')
-      }
-  }
-}
+
 
 
 })
